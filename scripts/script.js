9122ace042a41c732b90078da720c69c2e57e41c
Anna Mikheeva <Lishudi@yandex.ru>
2022-01-17 01:07:39 +0300

    Первоначальная верстка Профиль

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+const popupOpenButton = document.querySelector('.profile__info-edit-button');
+const popupCloseButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+
+function openPopup(event) {
+  event.preventDefault();
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+
+popup.addEventListener('click', function(event) {
+  if (!event.defaultPrevented) {
+    closePopup()
+  }
+});
+
+popupContainer.addEventListener('click', function(event) {
+  event.preventDefault();
+});
+
+let formElement = document.querySelector('.popup__container-info-button');
+let nameInput = document.querySelector('.popup__container-info-name');
+let jobInput = document.querySelector('.popup__container-info-occupation');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+let nameUser = document.querySelector('.profile__info-name');
+let occupationUser = document.querySelector('.profile__info-occupation');
+nameUser.setAttribute('value', nameInput.value);
+occupationUser.setAttribute('value', jobInput.value);
+}
+
+formElement.addEventListener('click', formSubmitHandler); 
