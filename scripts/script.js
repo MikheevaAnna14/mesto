9131349a12e4d7b1e8f7e0e7ee9c0dcc1105ec2b
Anna Mikheeva <Lishudi@yandex.ru>
2022-01-25 14:47:17 +0300

    Корр после ревью 1

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,17 @@ let popupOpenButton = document.querySelector('.profile__info-edit-button');
 let popupCloseButton = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
 let popupContainer = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__container-info-button');
+let nameInput = document.querySelector('.popup__container-input_name');
+let jobInput = document.querySelector('.popup__container-input_occupation');
+let nameUser = document.querySelector('.profile__info-name');  
+let occupationUser = document.querySelector('.profile__info-occupation');
 
 function openPopup(event) {
   event.preventDefault();
   popup.classList.add('popup_opened');
+  nameInput.value = nameUser.textContent;
+  jobInput.value = occupationUser.textContent;
 }
 
 function closePopup(event) {
@@ -13,25 +20,20 @@ function closePopup(event) {
   popup.classList.remove('popup_opened');
 }
 
+function formSubmitHandler(evt) {
+  nameUser.textContent = nameInput.value;
+  occupationUser.textContent = jobInput.value;
+  closePopup(evt);
+}
+
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+popupContainer.addEventListener('submit', formSubmitHandler);
+
+
 
 // popup.addEventListener('click', function(event) {
 //   if (!event.defaultPrevented) {
 //     closePopup();
 //   }
 // });
-
-let formElement = document.querySelector('.popup__container-info-button');
-let nameInput = document.querySelector('.popup__container-info-name');
-let jobInput = document.querySelector('.popup__container-info-occupation');
-
-function formSubmitHandler(evt) {
-  let nameUser = document.querySelector('.profile__info-name');  
-  let occupationUser = document.querySelector('.profile__info-occupation');
-  nameUser.textContent = nameInput.value;
-  occupationUser.textContent = jobInput.value;
-  closePopup(evt);
-}
-
-popupContainer.addEventListener('submit', formSubmitHandler);
