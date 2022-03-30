3eaee4948584c20501e304e5128234de88f8f261
Anna Mikheeva <Lishudi@yandex.ru>
2022-03-30 21:03:40 +0300

    рефакторинг и папка components

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 import { initialCards } from "./initialCards.js";
-import { Card } from "./card.js";
-import { FormValidator} from "./FormValidator.js";
-import { Section  } from "./Section.js";
-import { PopupWithImage } from "./PopupWithImage.js";
-import { PopupWithForm } from "./PopupWithForm.js";
-import { UserInfo } from "./UserInfo.js";
+import { Card } from "./components/card.js";
+import { FormValidator} from "./components/FormValidator.js";
+import { Section  } from "./components/Section.js";
+import { PopupWithImage } from "./components/PopupWithImage.js";
+import { PopupWithForm } from "./components/PopupWithForm.js";
+import { UserInfo } from "./components/UserInfo.js";
 
 const popupOpenButton = document.querySelector('.profile__info-edit-button');
 const popupForm = document.querySelector('#popupProfileForm');
